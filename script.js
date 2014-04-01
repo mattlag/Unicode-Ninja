5e32c4dbe79c4cac1955b436e6e3963cd7c87c0e
mattlag <mattlag@gmail.com>
2014-04-01 10:53:54 -0700

    Split codes into 4 tabs - seems to be working

--- script.js
+++ script.js
@@ -1,52 +1,102 @@
 
-	var nametable = false;
-	var numtable = false;
-	var numtabledone = false;
-	var numrow = "";
+	var tabcontent = [false,false,false,false,false];
+	var tablestyle = {
+		"size": 24,
+		"family": "sans-serif"
+	};
 
 	function init(){
-		tab(true);
+
+		// Make Name Table
+		tabcontent[0] = "<table id='symboltable'><tr>";
+		for(s=0; s<namearr.length; s++){
+			if(s%16===0){ tabcontent[0] += "</tr><tr>"; }
+			tabcontent[0] += "<td>"+makeOneSquare(namearr[s])+"</td>";
+		}
+
+		// Navigate
+		tab(0);
 		clickSymbol('amp');
 
-		numrow = "<td class='label'>&nbsp;</td>";
+		// Make other tables later
+		setTimeout(makeNumericTables, 50);
+	}
+
+	function tab(t){
+		document.getElementById('content').innerHTML = "<div style='width:100%; font-family:arial;'>Loading character codes...</div>";
+		document.getElementById('tab0').style.backgroundColor = "rgb(225,234,240)";
+		document.getElementById('tab1').style.backgroundColor = "rgb(225,234,240)";
+		document.getElementById('tab2').style.backgroundColor = "rgb(225,234,240)";
+		document.getElementById('tab3').style.backgroundColor = "rgb(225,234,240)";
+		document.getElementById('tab4').style.backgroundColor = "rgb(225,234,240)";
+		document.getElementById('tab'+t).style.backgroundColor = 'white';
+
+
+		var seltab = parseInt(t);
+
+		var updateContent = function(){
+			console.log("loading tab " + seltab);
+			if(tabcontent[seltab]){
+				document.getElementById('content').innerHTML = tabcontent[seltab];	
+				changeFontSize(tablestyle.size);
+				changeFontFamily(tablestyle.family);
+			} else {
+				setTimeout(updateContent, 500);
+			}
+		};
+
+		setTimeout(updateContent, 50);
+	}
+
+	function makeNumericTables(){
+		var temptable = "";
+
+		// Make numrow variable
+		var numrow = "<td class='label'>&nbsp;</td>";
 		for(var r=0; r<16; r++){ numrow += "<td class='label'>"+decToHex(r).charAt(3)+"</td>"; }
 
-		setTimeout(makeNumericTable, 50);
-	}
-
-	function popNameTab(){
-		if(!nametable){
-			nametable = "<table id='symboltable'><tr>";
-			for(s=0; s<namearr.length; s++){
-				if(s%16===0){ nametable += "</tr><tr>"; }
-				nametable += "<td>"+makeOneSquare(namearr[s])+"</td>";
-			}
+		// 0000 - 3FFF
+		temptable = "<table id='symboltable'><tr>";
+		for(var n=0x0000; n<(0x4000); n++){
+			if(n%0x100===0) temptable += "</tr><tr>"+numrow;
+			if(n%0x10===0) temptable += "</tr><tr><td class='label'>x"+decToHex(n)+"</td>";
+			temptable += "<td>"+makeOneSquare("#x"+decToHex(n))+"</td>";
 		}
-		document.getElementById('content').innerHTML = nametable;
-	}
+		temptable += "</tr></table>";
+		tabcontent[1] = temptable;
 
-	function popNumericTab(){
-		document.getElementById('content').innerHTML = "<div style='width:100%'>Loading 65,535 character codes...</div>";
-		console.log("popNumericTab - numtabledone = " + numtabledone);
-		if(numtabledone){
-			setTimeout(function(){document.getElementById('content').innerHTML = numtable;}, 50);
-		} else {
-			setTimeout(popNumericTab, 500);
+
+		// 4000 - 7FFF
+		temptable = "<table id='symboltable'><tr>";
+		for(var m=0x4000; m<(0x8000); m++){
+			if(m%0x100===0) temptable += "</tr><tr>"+numrow;
+			if(m%0x10===0) temptable += "</tr><tr><td class='label'>x"+decToHex(m)+"</td>";
+			temptable += "<td>"+makeOneSquare("#x"+decToHex(m))+"</td>";
 		}
-	}
+		temptable += "</tr></table>";
+		tabcontent[2] = temptable;
 
-	function makeNumericTable(){
-		numtable = "<table id='symboltable'><tr>";
 
-		for(var n=0x0000; n<=(0xFFFF); n++){
-			if(n%0x1000===0) console.log("makeNumericTable - done through " + decToHex(n));
-			if(n%0x100===0) numtable += "</tr><tr>"+numrow;
-			if(n%0x10===0) numtable += "</tr><tr><td class='label'>x"+decToHex(n)+"</td>";
-			numtable += "<td>"+makeOneSquare("#x"+decToHex(n))+"</td>";
+		// 8000 - BFFF
+		temptable = "<table id='symboltable'><tr>";
+		for(var p=0x8000; p<(0xC000); p++){
+			if(p%0x100===0) temptable += "</tr><tr>"+numrow;
+			if(p%0x10===0) temptable += "</tr><tr><td class='label'>x"+decToHex(p)+"</td>";
+			temptable += "<td>"+makeOneSquare("#x"+decToHex(p))+"</td>";
 		}
-		numtable += "</tr></table>";
+		temptable += "</tr></table>";
+		tabcontent[3] = temptable;
 
-		numtabledone = true;
+
+		// C000 - FFFF
+		temptable = "<table id='symboltable'><tr>";
+		for(var q=0xC000; q<=(0xFFFF); q++){
+			if(q%0x100===0) temptable += "</tr><tr>"+numrow;
+			if(q%0x10===0) temptable += "</tr><tr><td class='label'>x"+decToHex(q)+"</td>";
+			temptable += "<td>"+makeOneSquare("#x"+decToHex(q))+"</td>";
+		}
+		temptable += "</tr></table>";
+		tabcontent[4] = temptable;
 	}
 
 	function decToHex(d) { var dr = Number(d).toString(16); while(dr.length < 4) { dr = "0"+dr; } return dr.toUpperCase(); }
@@ -56,28 +106,19 @@
 		return re;
 	}
 
-	function tab(t){
-		if(t){
-			popNameTab();
-			document.getElementById("nametab").style.backgroundColor = "white";
-			document.getElementById("numerictab").style.backgroundColor = "rgb(230,230,230)";
-		} else {
-			popNumericTab();
-			document.getElementById("nametab").style.backgroundColor = "rgb(230,230,230)";
-			document.getElementById("numerictab").style.backgroundColor = "white";
-		}
-	}
-
 	function clickSymbol(sym){
 		var re = "<input class='showclickleft' value='&"+sym+";'><input class='showclickright' type='text' value='&amp;"+sym+";'>";
 		document.getElementById("showclick").innerHTML = re;
 	}
 
 	function changeFontSize(val){
+		tablestyle.size = val;
 		document.getElementById('symboltable').style.fontSize = parseInt(val)+'px';
 	}
 
 	function changeFontFamily(val){
+		tablestyle.family = val;
+		if(val.indexOf(' ')>-1) val = ("'"+val+"'");
 		document.getElementById('symboltable').style.fontFamily = val;
 	}
 
